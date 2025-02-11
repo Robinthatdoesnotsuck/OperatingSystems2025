4550b49d281e1c153a9c329275e2770c86533daf
Iram Lopez <ilopez@qualtrics.com>
2025-02-11 15:58:42 -0800

    updated something i guess

---
+++ SystemProgramming/BasicSystemCalls/Threads/threads.cpp
@@ -0,0 +1,38 @@
+
+
+char * buffer = sizeof(char) * 8;
+
+void suma() {
+  hacer otras cosas
+  mutex.lock()
+
+
+
+
+
+
+
+  sumar
+  
+  mutex.unlock()
+}
+
+void resta() {
+  hacer otras cosas
+  mutex.lock()
+  restar
+  mutex.unlock()
+}
+
+
+int main (int argc, char *argv[]) {
+  // comportamiento salvaje}
+  // Comportamiento indeterminado
+  //
+  new Thread(suma);
+  new Thread(resta);
+
+  return 0;
+}
+
+
