f0a31e5baf4fbe5e9d75828b525995d41643afa1
Robinthatdoesnotsuck <iram644@hotmail.com>
2026-01-21 19:46:20 -0600

    Added first week stuff

--- SystemProgramming/BasicSystemCalls/Threads/threads.cpp
+++ SystemProgramming/BasicSystemCalls/Threads/threads.cpp
@@ -1,38 +1,22 @@
 
+malloc()
 
-char * buffer = sizeof(char) * 8;
-
-void suma() {
-  hacer otras cosas
-  mutex.lock()
-
+stack[]
+heap[]
+0x6464FA
 
 
-
-
-
-
-  sumar
-  
-  mutex.unlock()
+int sumar(int a, int b) {
+	return a + b;
 }
 
-void resta() {
-  hacer otras cosas
-  mutex.lock()
-  restar
-  mutex.unlock()
+int restar(int a, int b) {
+  return a - b;
+}
+int main() {
+  sumar()	
+  restar()
 }
 
-
-int main (int argc, char *argv[]) {
-  // comportamiento salvaje}
-  // Comportamiento indeterminado
-  //
-  new Thread(suma);
-  new Thread(resta);
-
-  return 0;
-}
 
 
