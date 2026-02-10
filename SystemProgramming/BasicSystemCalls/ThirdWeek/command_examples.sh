52d7f58fbdcf802e554be5de50d599e712eea82d
Robinthatdoesnotsuck <iram644@hotmail.com>
2026-02-09 19:21:50 -0600

    Third week first part

---
+++ SystemProgramming/BasicSystemCalls/ThirdWeek/command_examples.sh
@@ -0,0 +1,40 @@
+man rm
+whatis rm
+man rm
+man ls
+clear
+history
+clear
+history | grep cd
+clear
+pip freeze > requirements.txt; cat requirements.txt | grep pandas
+ls
+cat requirements.txt 
+man grep
+grep x requirements.txt
+cp requirements.txt requirements2.txt 
+grep x requirements.txt requirements2.txt 
+clear
+wc
+man wc
+clear
+wc -l requirements
+wc -l requirements.txt
+wc -w requirements.txt
+cat requirements.txt 
+grep -*==1 requirements
+grep -*==1 requirements.txt
+grep ==1 requirements.txt
+grep ==1 requirements.txt | wc -l
+clear
+grep ==1 requirements.txt
+grep ==1 requirements.txt | wc -l
+grep ==0 requirements.txt | wc -l
+clear
+cat > example.cpp
+man cat
+cat example.cpp 
+cat > example.cpp
+cat example.cpp 
+cat >> example.cpp 
+cat example.cpp 
